9dae091c8725a3d8435dcf28c6e4a4ab91c1f503
Seifeldin Adel Said <62226196+seifadel25@users.noreply.github.com>
2024-02-06 12:34:08 +0200

    Update server.js

--- server.js
+++ server.js
@@ -154,6 +154,18 @@ async function fetchDataFromAPI2() {
       .slice(0, 15);
 
     const modifiedArticles = articles.map((article) => {
+      if (article.date) {
+        // Convert the ISO date string to a Date object
+        const date = new Date(article.date);
+        // Format the date as desired, e.g., "DD-MM-YYYY HH:MM"
+        const day = date.getDate().toString().padStart(2, '0');
+        const month = (date.getMonth() + 1).toString().padStart(2, '0'); // Month is 0-indexed; +1 to adjust
+        const year = date.getFullYear();
+        const hours = date.getHours().toString().padStart(2, '0');
+        const minutes = date.getMinutes().toString().padStart(2, '0');
+        // Construct the custom formatted date string
+        article.DateF = `${day}-${month}-${year} ${hours}:${minutes}`;
+      }
       if (article.url) {
         article.url = modifyUrl(article.url);
       }
